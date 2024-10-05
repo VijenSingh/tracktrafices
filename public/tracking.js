775e3c07fb3e2d5d8a1a5a8be70f7721da0b626b
vijju <vijju@vijjus-MacBook-Air.local>
2024-10-05 13:46:21 +0530

    05-pixel

--- public/tracking.js
+++ public/tracking.js
@@ -1,107 +1,76 @@
-(async () => {
+(async function() {
     
-    let dynamicUrl = '';
-
-    try {
-        const response = await fetch('https://www.tracktraffics.com/getTrackingUrl');
-        const data = await response.json();
-        dynamicUrl = data.trackingUrl; 
-        console.log("dynamicUrl 9=> ",dynamicUrl)
-    } catch (error) {
-        console.error('Error fetching tracking URL:', error);
-        return; 
+    function generateUUID() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random() * 16 | 0,
+                v = c === 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
     }
 
-    let trackingUrl = new URL(dynamicUrl);
 
-
-    let offerId = trackingUrl.searchParams.get("offer_id");
-    let affId = trackingUrl.searchParams.get("aff_id");
-
-    console.log(`Offer ID: ${offerId}, Affiliate ID: ${affId}`);
-
-    let currentUrl = window.location.href;
-    let referrer = document.referrer;
-
-  
-    const cookieName = "__rock_fingerprint";
-    const expirationDate = new Date(Date.now() + 2592e6).toUTCString();
-    let guestFingerprint = getCookie(cookieName);
-    
-    if (!guestFingerprint) {
-        guestFingerprint = generateUUID();
-        document.cookie = `${cookieName}=${guestFingerprint}; expires=${expirationDate}; path=/`;
+    function createTrackingPixel(url) {
+        
+        var img = document.createElement('img');
+        img.src = url;
+        img.style.width = '1px';
+        img.style.height = '1px';
+        img.style.display = 'none';  
+        img.style.visibility = 'hidden';
+        
+        document.body.appendChild(img);
     }
 
-    let trackingData = {
-        url: currentUrl,
-        referrer: referrer,
-        uuid: guestFingerprint,
-        offerId: offerId,
-        affId: affId,
-        origin: window.location.hostname,
-    };
+    async function initTracking() {
+        if (sessionStorage.getItem('iframe_triggered')) {
+            return; 
+        }
 
-   
-    let backendUrl = "https://www.tracktraffics.com/aff_retag"; 
-    fetch(backendUrl, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(trackingData)
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log("data 56 => ",data)
-        if (data.error === "success") {
-          
-            console.log("Tracking data sent successfully", data);
-            if (data.data) {
-                var tempDiv = document.createElement("div");
-                tempDiv.innerHTML = data.data;
+        try {
+            let uniqueId = getCookie('tracking_uuid') || generateUUID();
+            let expires = (new Date(Date.now() + 30 * 86400 * 1000)).toUTCString();
+            document.cookie = 'tracking_uuid=' + uniqueId + '; expires=' + expires + ';path=/;';
 
-                var scripts = tempDiv.querySelectorAll("script");
-                if (scripts) {
-                    for (const script of scripts) {
-                        if (script.src) {
-                            var newScript = document.createElement("script");
-                            newScript.src = script.src;
-                            script.id && (newScript.id = script.id);
-                            script.async && (newScript.async = script.async);
-                            script.defer && (newScript.defer = script.defer);
-                            document.head.appendChild(newScript);
-                        } else {
-                            var inlineScript = document.createElement("script");
-                            inlineScript.textContent = script.textContent;
-                            script.type && (inlineScript.type = script.type);
-                            document.head.appendChild(inlineScript);
-                        }
-                    }
+            let response = await fetch('https://www.tracktraffics.com/api/track-user', {
+                method: 'POST',
+                body: JSON.stringify({
+                    url: window.location.href,
+                    referrer: document.referrer,
+                    unique_id: uniqueId,
+                    origin: window.location.hostname,
+                }),
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin':'*'
                 }
-            }
-        } else {
-            console.error("Error in tracking response:", data);
-        }
-    })
-    .catch(err => console.error("Fetch error:", err));
-})();
+            });
 
-function getCookie(name) {
-    const cookieArr = document.cookie.split(";");
-    for (let i = 0; i < cookieArr.length; i++) {
-        let cookiePair = cookieArr[i].split("=");
-        if (name === cookiePair[0].trim()) {
-            return decodeURIComponent(cookiePair[1]);
+            let result = await response.json();
+            if (result.success && result.affiliate_url) {
+                createTrackingPixel(result.affiliate_url);
+                sessionStorage.setItem('iframe_triggered', 'true'); 
+            } else {
+                createTrackingPixel('https://www.tracktraffics.com/api/fallback-pixel?id=' + uniqueId);
+            }
+        } catch (error) {
+            console.error('Error in tracking script:', error);
         }
     }
-    return null;
-}
 
-function generateUUID() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-        let r = Math.random() * 16 | 0,
-            v = c === 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    });
-}
+    function getCookie(cname) {
+        var name = cname + '=';
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) === ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) === 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return '';
+    }
+
+    initTracking()
+})();
